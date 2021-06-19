896794c8d46f9fd389049a83879b0decaeb31e6e
Krishna Kant Sharma <31980947+krishna7860@users.noreply.github.com>
2021-06-19 14:12:18 +0530

    Added the Sentry Loggin and Peformance Profiling (#14)
    Co-authored-by: Krishna Sharma <krishna.sharma@shipsy.co.in>

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { createBrowserHistory } from "history";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const history = createBrowserHistory();
+
+Sentry.init({
+  dsn: "https://a24a1f9f37254da28cae7ee56e5f96f7@o859938.ingest.sentry.io/5820830",
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1,
+});
+
+const ProfileApp = Sentry.withProfiler(App);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Sentry.ErrorBoundary fallback="An error has occurred">
+    <React.StrictMode>
+      <ProfileApp />
+    </React.StrictMode>
+  </Sentry.ErrorBoundary>,
   document.getElementById("root")
 );
 
